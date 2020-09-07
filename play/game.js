d7ff8be9dc57c229dc9455695db26de5a5372d38
Frank-9976 <41272099+Frank-9976@users.noreply.github.com>
2020-09-07 01:25:27 -0700

    Update game.js

--- play/game.js
+++ play/game.js
@@ -1,1 +1,17 @@
-alert("Hello, World!");
+const friendlyCardArea = document.getElementById('friendlycardarea');
+
+function addCard(el, cardData) {
+  const newCard = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  newCard.setAttribute("width", "100");
+  newCard.setAttribute("height", "100");
+
+  const dummyCardText = document.createElementNS("http://www.w3.org/2000/svg", "text");
+  dummyCardText.setAttribute("x", 50);
+  dummyCardText.setAttribute("y", 50);
+  dummyCardText.textContent = cardData.weed;
+  newCard.appendChild(dummyCardText);
+  
+  el.appendChild(newCard);
+}
+
+addCard({weed: "420"});
