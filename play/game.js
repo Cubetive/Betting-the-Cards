5c4de8dc6eb496aabfcdb31d97f1a7dbc83b3461
Frank-9976 <41272099+Frank-9976@users.noreply.github.com>
2020-09-07 21:35:48 -0700

    Update game.js

--- play/game.js
+++ play/game.js
@@ -1,10 +1,19 @@
 const friendlyCardArea = document.getElementById('friendlycardarea');
+const STANDARD_CARD_SIZE = 100;
 
 function addCard(el, cardData) {
   const newCard = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-  newCard.setAttribute("width", "100");
-  newCard.setAttribute("height", "100");
+  let cardSize = STANDARD_CARD_SIZE;
+  newCard.setAttribute("width", cardSize);
+  newCard.setAttribute("height", cardSize);
 
+  const cardBorder = document.createElementNS("http://www.w3.org/2000/svg", "rectangle");
+  cardBorder.setAttribute("width", cardSize);
+  cardBorder.setAttribute("height", cardSize);
+  cardBorder.setAttribute("fill-opacity", 0);
+  cardBorder.setAttribute("stroke", "black");
+  newCard.appendChild(cardBorder);
+  
   const dummyCardText = document.createElementNS("http://www.w3.org/2000/svg", "text");
   dummyCardText.setAttribute("x", 50);
   dummyCardText.setAttribute("y", 50);
