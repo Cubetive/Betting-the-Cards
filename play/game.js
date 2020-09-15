350c90f6ab62c416f6d3d6c96be1637d728bad17
Cubetive <tuanminh@Tuans-MacBook-Pro.local>
2020-09-15 09:52:34 +0700

    Basic interface

--- play/game.js
+++ play/game.js
@@ -12,15 +12,15 @@ function addCard(el, cardData) {
   cardBorder.setAttribute("height", "100%");
   cardBorder.setAttribute("style", "fill:lightgrey;stroke:black;stroke-width:3");
   newCard.appendChild(cardBorder);
-  
+
   const dummyCardText = document.createElementNS("http://www.w3.org/2000/svg", "text");
   dummyCardText.setAttribute("x", 50);
   dummyCardText.setAttribute("y", 50);
   dummyCardText.textContent = cardData.weed;
   newCard.appendChild(dummyCardText);
-  
+
   el.appendChild(newCard);
 }
 
 addCard(friendlyCardArea, {weed: "420"});
-// funny number!!!!
+// funny number!!!! no
