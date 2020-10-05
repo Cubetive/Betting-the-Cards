8ed6c8b8944398c96b109126ffc14597ff0294c8
Cubetive <tuanminh@Tuans-MacBook-Pro.local>
2020-10-05 15:14:19 +0700

    Fixed a few files + add more files

--- play/game.js
+++ play/game.js
@@ -27,37 +27,4 @@ var enemy = {
   deck: []
 }
 
-// Card Management
-const friendlyCardArea = document.getElementById('friendlycardarea');
-const enemyCardArea = document.getElementById('enemyCardArea');
-const STANDARD_CARD_WIDTH = 150;
-const STANDARD_CARD_HEIGHT = 200;
-
-function addCard(el, cardData) {
-  const newCard = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-  let cardSizeWidth = STANDARD_CARD_WIDTH;
-  let cardSizeHeight = STANDARD_CARD_HEIGHT;
-  newCard.setAttribute("width", cardSizeWidth);
-  newCard.setAttribute("height", cardSizeHeight);
-
-  const cardBorder = document.createElementNS("http://www.w3.org/2000/svg", "rect");
-  cardBorder.setAttribute("width", "100%");
-  cardBorder.setAttribute("height", "100%");
-  cardBorder.setAttribute("style", "fill:lightgrey;stroke:black;stroke-width:3");
-  newCard.appendChild(cardBorder);
-
-  const name = document.createElementNS("http://www.w3.org/2000/svg", "text");
-  name.setAttribute("x", "50%");
-  name.setAttribute("y", (STANDARD_CARD_HEIGHT / 2) + (STANDARD_CARD_HEIGHT / 4));
-  name.setAttribute("transform", "translateX(-50%)")
-  name.textContent = cardData.name;
-  newCard.appendChild(name);
-  el.appendChild(newCard);
-}
-
-async function UpdateFrame() {
-
-}
-
-addCard(friendlyCardArea, {name: "Tran Dan"});
-// funny number!!!! no
+// Main
